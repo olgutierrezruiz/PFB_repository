660f7f1be4dc1326fc052b07dc60210c573121f5
student <student@info-03.cshl.edu>
2022-10-11 11:59:45 -0400

    update problemset3

--- problemset3.py
+++ problemset3.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-print('My name: Omar')
+import sys
+name = sys.argv[1]
+color = sys.argv[2]
+hobby = sys.argv[3]
+animal = sys.argv[4]
+print('My name: ' + name + '\nMy favorite color: ' + color + '\nMy favorite activity: ' + hobby + '\nMy favorite animal: ' +animal)
+print('my favorite activity:' , hobby)
+print('my favorite animal: lion')
