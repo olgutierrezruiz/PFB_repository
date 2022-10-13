5378fdee2f47087188122581c8eb212959747a7b
student <student@info-03.cshl.edu>
2022-10-12 22:04:19 -0400

    problemset4_10-12-22.py

---
+++ problemset4_10-12-22.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#Question 1
+fav = ['lor', 'science', 'cancer', 'lysosomes', 'beer']
+print(fav)
+print(fav[2])
+
+fav[2] = 'speedofsound'   #replaces the third element in the list by speedofsound
+print(fav)
+
+fav.append('thekillers')  #adds an element at the end of the list
+fav.insert(0,'keane')     #adds an element at the begining of the list
+fav.insert(3,'wolfgang')	#adds an element at position 3
+fav.pop()									#removes the last element of a list
+
+
+
+
+
+
+
